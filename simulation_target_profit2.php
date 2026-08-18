a12d62030ef530da06059cb3ac4b9f9b64ad21ea
cvstecks-cmd <cvstecks@gmail.com>
2026-08-18 10:08:49 +0100

    Create simulation_target_profit2.php

---
+++ simulation_target_profit2.php
@@ -0,0 +1,15 @@
+$target =
+                round(
+                    (float)
+                    $lockedSubscription
+                        ->simulation_target_profit,
+                    8
+                );
+
+            $capital =
+                round(
+                    (float)
+                    $lockedSubscription
+                        ->amount,
+                    8
+                );
